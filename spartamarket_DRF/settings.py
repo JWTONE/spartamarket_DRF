1ae211cd7eff20514e1e6c3646f857ccef6bd5c6
Leejungwoo <soulpc123@gmail.com>
2024-04-29 19:44:20 +0900

    git ignore config 적용

--- spartamarket_DRF/settings.py
+++ spartamarket_DRF/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from .config import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django_seed',
     'accounts',
     'products',
+    'django-restfulframework'
 ]
 
 MIDDLEWARE = [
