c4cfab145473b370b94eb5f638d6f38f793d44ea
Leejungwoo <soulpc123@gmail.com>
2024-04-30 16:30:36 +0900

    delete 기능 완료

--- products/views.py
+++ products/views.py
@@ -1,16 +1,8 @@
-from functools import partial
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.request import Request
-
-from accounts import serializers
 from .serializers import ProductsSerializer
 from .models import Products
 
@@ -37,3 +29,11 @@ class ProductsListAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, productID):
+        product = get_object_or_404(Products, id=productID)
+        
+        if product.username == request.user:
+            product.delete()
+            return Response(status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
